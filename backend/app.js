e6a72f790ca77b6ea4b2cc663cdb39411dc3a405
AbhayLodhi24 <abhaylodhi24@gmail.com>
2024-08-20 23:58:55 +0530

    modified app.js

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const app = express();
 dotenv.config();
 conn();
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: [process.env.FRONTEND_URL],
     credentials: true,
 }));
 app.use(express.json());
